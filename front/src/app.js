0093888faf4f56d4b4b4d8c6823c4ae752e31954
ys <644609238@qq.com>
2017-12-20 13:18:21 +0800

    引入蚂蚁金服

--- front/src/app.js
+++ front/src/app.js
@@ -2,20 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {Router, Route, hashHistory, browserHistory, Link, IndexRoute} from 'react-router'
 
-
-import RootComponent from './components/rootComponent';
-import NavComponent from './components/navComponent';
-import MovieComponent from './components/movieComponent';
-import TvComponent from './components/tvComponent';
-import ShowComponent from './components/showComponent';
+// 引入蚂蚁金服
+import { DatePicker } from 'antd';
+import 'antd/dist/antd.css';
 
 
+
+
+
+     
 ReactDOM.render(
-    <Router history={hashHistory}>
-        <Route path="/" component={RootComponent}></Route>
-        <Route path="/nav" component={NavComponent}>
-            <Route path="movie" component={MovieComponent}></Route>
-            <Route path="tv" component={TvComponent}></Route>
-            <Route path="show" component={ShowComponent}></Route>
-        </Route>
-    </Router>,document.getElementById('app'))
+    <div>
+        
+    </div>
+, document.getElementById('app'));
