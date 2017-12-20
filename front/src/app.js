f2d1c609b4fdbfcfeb680267be73dc2cd9eb6147
ZLinC <1109502057@qq.com>
2017-12-20 20:43:51 +0800

    部分改动

--- front/src/app.js
+++ front/src/app.js
@@ -9,11 +9,14 @@ import './basescss/base.scss'
 import Rem from './getRem/getRem.js'
 import Cart from './components/shoppingCart/shoppingCartComponent'
 import ClassifyComponent from './components/csm_classify/classifyComponent.js'
+
+import Login from './components/login/loginComponent'
 Rem.rem();
 ReactDOM.render(
     <Router history={hashHistory}>
         <Route path="/Classify" component={ClassifyComponent}/>
         <Route path="/cart" component={Cart}/>
+        <Route path="/login" component={Login}/>
     </Router>,document.getElementById('app'))
 
      
