d650e2b700e0dd0861714ba6a9e999f87a509add
sherah <2359932265@qq.com>
2017-12-20 17:27:24 +0800

    修改

--- front/src/app.js
+++ front/src/app.js
@@ -4,22 +4,19 @@ import {Router, Route, hashHistory, browserHistory, Link, IndexRoute} from 'reac
 import Rem from './getRem/getRem.js'
 import './basescss/base.scss'
 import ClassifyComponent from './components/csm_classify/classifyComponent.js'
-
-<<<<<<< HEAD
-Rem.rem();
-ReactDOM.render(
-    <Router history={hashHistory}>
-        <Route path="/" component={ClassifyComponent}/>
-    </Router>,document.getElementById('app'))
-=======
 // 引入蚂蚁金服
 import { DatePicker } from 'antd';
 import 'antd/dist/antd.css';
 
 import Cart from './components/shoppingCart/shoppingCartComponent'
 
-
+Rem.rem();
+ReactDOM.render(
+    <Router history={hashHistory}>
+        <Route path="/" component={ClassifyComponent}/>
+        <Route path="/cart" component={Cart}/>
+    </Router>,document.getElementById('app'))
 
      
-ReactDOM.render(<Cart/>, document.getElementById('app'));
->>>>>>> cbe7c7571b765af7d97d9d5e54a16dcea7919d26
+
+
