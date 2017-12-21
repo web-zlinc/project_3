0b7352bb2d341d1ec649ed47bcb84b948be90f9f
Leon <490868429@qq.com>
2017-12-21 11:07:58 +0800

    更新了公共组件的样式

--- front/src/components/commonComponent/commonSearch.js
+++ front/src/components/commonComponent/commonSearch.js
@@ -4,38 +4,61 @@ const { Header, Content } = Layout;
 const Search = Input.Search;
 const ButtonGroup = Button.Group;
 
+import '../../assets/css/commonSearch.css'
+
 export default class SearchComponent extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            searchFruit: '',
+        };
+    }
+    emitEmpty() {
+        this.searchFruitInput.focus();
+        this.setState({ searchFruit: '' });
+    }
+    onChangesearchFruit(e) {
+        this.setState({ searchFruit: e.target.value });
+    }
     render() {
+        const { searchFruit } = this.state;
+        const suffix = searchFruit ? <Icon type="close-circle" onClick={this.emitEmpty.bind(this)} /> : null;
         return (
             <div>
-                <Layout>
-                    <Header style={{ background: '#fff' }}>
-                        <ButtonGroup>
-                            <Button>
-                                <Icon type="left" />返回
-                            </Button>
-                        </ButtonGroup>
-                        <Search
-                            placeholder="新鲜水果"
-                            onSearch={value => console.log(value)}
-                            style={{ width: '83%' }}
+                <Layout id="commonSearch">
+                    <Header id="searchNav" style={{ background: '#fff' }}>
+                        <div className="navbar-header">
+                            <Icon type="right" />
+                            <span>返回</span>
+                        </div>
+                        <Input
+                            id="searchInput"
+                            placeholder="新鲜水果来一波"
+                            prefix={<Icon type="search" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                            suffix={suffix}
+                            value={searchFruit}
+                            onChange={this.onChangesearchFruit.bind(this)}
+                            ref={node => this.searchFruitInput = node}
                         />
-                        <ButtonGroup>
-                            <Button>
-                                搜索
-                            </Button>
-                        </ButtonGroup>
+                        <div className="sub-btn">
+                            <span>
+                            搜索
+                            </span>
+                        </div>
                     </Header>
-                    <Content style={{ padding: '20px 50px' }} >
-                        <p>热门搜索：</p>
-                        <Row >
-                            <Col span={2}><Button>橙子</Button></Col>
-                            <Col span={2}><Button>提子</Button></Col>
-                            <Col span={2}><Button>樱桃</Button></Col>
-                            <Col span={2}><Button>苹果</Button></Col>
-                            <Col span={2}><Button>瓜果</Button></Col>
-                        </Row>
-
+                    <Content id="keyword" style={{ padding: '20px 50px' }} >
+                        <div className="title">热门搜索：</div>
+                        <div className="con">  
+                            <a>橙</a>  
+                            <a>提子</a>  
+                            <a>樱桃</a>  
+                            <a>苹果</a>  
+                            <a>火龙果</a>  
+                            <a>瓜</a>  
+                            <a>羊蝎子</a>  
+                            <a>雪花鲳</a>  
+                            <a>奇异果</a>  
+                        </div>
                     </Content>
                 </Layout>
             </div>
