8a4e533c6ac09d9f006aba3860d1aed9d7191da8
ZLinC <1109502057@qq.com>
2017-12-21 08:43:39 +0800

    登录页修改

--- front/src/components/login/loginComponent.js
+++ front/src/components/login/loginComponent.js
@@ -13,7 +13,22 @@ export default class LoginComponent extends React.Component{
                     </p>
                 </div>
                 <div className="l_main">
-
+                    <p>
+                        <Icon type="mobile" />
+                        <Input placeholder="手机号" className="phone" />
+                       
+                    </p>
+                    <p>
+                        <Icon type="key" />
+                        <Input placeholder="短信验证码" />
+                    </p>
+                    <p>
+                        <Icon type="lock" />
+                        <Input type="password" placeholder="密码" />
+                    </p>
+                    <p className="cont">首次用手机号登录将自动为您注册，并有豪礼相送。</p>
+                    <p><Button type="primary">登录</Button></p>
+                    <p className="footer"><span className="fl">账号密码登录</span><span className="fr">未收到验证码?</span></p>
                 </div>
             </div>
         )
