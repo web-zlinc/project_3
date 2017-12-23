a5c62acc8435c46db9992e68212c69fe6ce9f3be
Leon <490868429@qq.com>
2017-12-23 09:08:24 +0800

    公共组件处小修改

--- front/src/redux/rootReducer.js
+++ front/src/redux/rootReducer.js
@@ -1,22 +1,15 @@
 import {combineReducers} from 'redux'
-<<<<<<< HEAD
+
+import login from '../components/login/loginReducer'
 import datagrid from '../components/datagrid/datagridReducer'
 import allshow from '../components/csm/allShow/allShowReducer.js'
 import searchFruit from '../components/commonComponent/searchReducer'
+import cart from '../components/shoppingCart/shoppingCartReducer';
 
 export default combineReducers({
     datagrid,
     allshow,
-    searchFruit
-=======
-import login from '../components/login/loginReducer'
-import cart from '../components/shoppingCart/shoppingCartReducer';
-import allshow from '../components/csm/allShow/allShowReducer.js'
-
-
-export default combineReducers({
-    cart,
-    allshow,
-    login
->>>>>>> 10d7e9e7b0df1b74e8c6cb527f4de342f40491b8
+    searchFruit,
+    login,
+    cart
 })
